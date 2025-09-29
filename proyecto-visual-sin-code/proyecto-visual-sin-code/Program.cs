0ea1d91d05496b1d7b2799797dcc005d7c2cfa4a
unknown <danieltoro.programacion@gmail.com>
2025-09-28 20:35:48 -0500

    proyectos

---
+++ proyecto-visual-sin-code/proyecto-visual-sin-code/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+var papitas = new Venta(12);
+
+class Venta
+{
+    // Por convencion las Total con amyuscula
+    public decimal Total { get; set; }
+
+
+    // Los CONSTRUCTORES tiene el mismo nombre que la clase
+    public Venta ( decimal valorDeVenta )
+    {
+        this.Total = valorDeVenta;
+    }  
+}
+

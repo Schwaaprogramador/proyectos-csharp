dba1f4eb096a4e3f1e36f7351096fa8e06a0f502
Schwaaprogramador <frontd@alnago.com>
2025-10-17 14:45:55 -0500

    nuevos proyectos desde el escritorio del workeo

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int ladoA;
+
+Console.WriteLine("cuidadin liren");
+
+ladoA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ladoA);
---
+++ C#/circulo/circulo/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("bienvenido");
+
+Console.WriteLine("valor del radio");
+
+double radio = Convert.ToDouble(Console.ReadLine());
+
+double resultado = Math.PI * radio * radio;
+
+Console.WriteLine("El area es de " + resultado);
+
+
+const string PI = " tambien existen las constante en C# ";
+
+Console.WriteLine(  PI  );
